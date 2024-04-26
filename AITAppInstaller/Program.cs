5c3d5f6c26e17845c62b6881b68267f998e35eb6
Lydon Gray <lydongray@gmail.com>
2024-04-26 14:29:26 +1000

    fix: rename Form.cs to Form1.cs

--- AITAppInstaller/Program.cs
+++ AITAppInstaller/Program.cs
@@ -23,7 +23,7 @@ namespace AITAppInstaller
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new Form1());
         }
     }
 }
